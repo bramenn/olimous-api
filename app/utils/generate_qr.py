a8a0cef870287ef97eead759c73d750147c98dc6
bramen <bramendev@gmail.com>
2024-05-01 10:35:35 -0500

    Se agregan pruebas TDD

--- app/utils/generate_qr.py
+++ app/utils/generate_qr.py
@@ -1,8 +1,5 @@
 import uuid
 
 
-def qr_str(tournament_id: int, user_id: int, is_competitor: bool = True):
-    if is_competitor:
-        return f"{tournament_id}-{user_id}-{str(uuid.uuid4())}"
-
+def qr_str(tournament_id: int, user_id: int):
     return f"{tournament_id}-{user_id}-{str(uuid.uuid4())}"
