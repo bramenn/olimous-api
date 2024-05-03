255060f6f6a9d1c0573c262f64989f7240d2dd5a
bramen <bramendev@gmail.com>
2024-05-03 02:37:02 -0500

    Agregando documentos de documentación

--- app/db.py
+++ app/db.py
@@ -4,7 +4,16 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import POSTGRES_URI
 
-conn = create_engine(POSTGRES_URI, pool_pre_ping=True, pool_recycle=150)
+conn = create_engine(
+    POSTGRES_URI,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    },
+)
 
 Session = sessionmaker(bind=conn)
 
