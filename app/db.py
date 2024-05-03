e6aa21eb825d8adf02d499a2c95749d7f3939fe0
bramen <bramendev@gmail.com>
2024-05-02 19:23:10 -0500

    Agregando pre_ping a la conexión de la base de datos

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import POSTGRES_URI
 
-conn = create_engine(POSTGRES_URI, pool_pre_ping=True)
+conn = create_engine(POSTGRES_URI, pool_pre_ping=True, pool_recycle=150)
 
 Session = sessionmaker(bind=conn)
 
